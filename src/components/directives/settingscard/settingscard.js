85ea752a05a75f9f4f3c46fd0e6702ff8497a776
markroper <markroper@gmail.com>
2015-10-12 11:16:58 -0400

    Removes unused code from the settingscard.js link() method.

--- src/components/directives/settingscard/settingscard.js
+++ src/components/directives/settingscard/settingscard.js
@@ -12,25 +12,6 @@ angular.module('teacherdashboard')
       },
       restrict: 'E',
       templateUrl: api.basePrefix + '/components/directives/settingscard/settingscard.html',
-      replace: true,
-      link: function(scope, elem){
-        scope.element = elem;
-        //In order to make tooltip borders partially transparent, we have to 
-        //convert from hex to RGB because the visualization tool gives us colors as hex
-        scope.hexToRgb = function(hex) {
-            // Expand shorthand form (e.g. "03F") to full form (e.g. "0033FF")
-            var shorthandRegex = /^#?([a-f\d])([a-f\d])([a-f\d])$/i;
-            hex = hex.replace(shorthandRegex, function(m, r, g, b) {
-                return r + r + g + g + b + b;
-            });
-            var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
-            return result ? 
-                'rgba(' + 
-                parseInt(result[1], 16) + ', ' + 
-                parseInt(result[2], 16) + ', ' + 
-                parseInt(result[3], 16) + ', 0.8);'
-            : null;
-        };
-      }
+      replace: true
     };
   }]);
