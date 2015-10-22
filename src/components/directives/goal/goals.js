7ea03b88d7cc7b783a61b9f5ada8ab41dc85c615
cwallac <christopher.wallace@students.olin.edu>
2015-10-21 21:28:50 -0400

    Added directive test for slider

--- src/components/directives/goal/goals.js
+++ src/components/directives/goal/goals.js
@@ -12,9 +12,32 @@ angular.module('teacherdashboard')
         $scope.deleteGoal = function() {
           //Call api to delete the goal
         };
-        $scope.editGoal = function() {
-          //Call api to delete the goal
+        $scope.editGoal = function(goal) {
+          //Call api to edit the goal
+          goal.editActive = true;
+          goal.blah = Math.floor(Math.random() * 255);
+
         };
       }
     };
-  }]);
+  }])
+  .directive('testDragEnd', function() {
+    return {
+      restrict: 'A',
+      link: function(scope, element, attrs) {
+        console.log("IT LOADED");
+        element.on('$md.dragend', function() {
+          console.info('Drag Ended');
+        });
+        element.on('$md.drag', function() {
+          console.info('Drag Start');
+        });
+        element.on('$md.dragstart', function() {
+          console.info('Drag Start');
+        });
+        element.on('$md.pressdown', function() {
+          console.info('Drag Start');
+        })
+      }
+    }
+  });
