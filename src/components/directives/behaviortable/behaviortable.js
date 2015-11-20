4ed6eb40ef06925146dc11e2d080ff03b489aec0
markroper <markroper@gmail.com>
2015-11-20 08:59:08 -0500

    Updates to the behavior tab for small and large layouts, pagination on the table view, etc.

---
+++ src/components/directives/behaviortable/behaviortable.js
@@ -0,0 +1,32 @@
+'use strict';
+angular.module('teacherdashboard')
+  .directive('behaviortable', [ '$window', '$compile', '$sanitize','statebag', 'api', function($window, $compile, $sanitize, statebag, api) {
+    return {
+      scope: {
+        behaviorPromise: '=',
+      },
+      restrict: 'E',
+      templateUrl: api.basePrefix + '/components/directives/behaviortable/behaviortable.html',
+      replace: true,
+      controllerAs: 'ctrl',
+      link: function(scope, elem){
+        scope.behaviorData = [];
+        scope.tableConfig = {
+          data: 'behaviorData',
+          enableColumnMenus: false,
+          paginationPageSize: 7,
+          paginationPageSizes: [7, 15, 30],
+          enablePaginationControls: true,
+          columnDefs: [
+            { field: 'behaviorCategory' },
+            { field: 'behaviorDate', type: 'date', cellFilter: 'date:\'yyyy-MM-dd\'' },
+            { field: 'name' }
+          ]
+        };
+        scope.behaviorPromise.then(function(theData){
+          scope.behaviorData = theData;
+        });
+      }
+    }
+  }
+]);
