20262c13adae75d504f0b60f9fb8d424a6781e91
markroper <markroper@gmail.com>
2016-03-08 08:42:27 -0500

    Adds a gridster dashboard layout builder.  Binds it to the dashboard object returned in the API.

--- src/components/directives/dashboard/createEditDashboard.js
+++ src/components/directives/dashboard/createEditDashboard.js
@@ -9,14 +9,45 @@ angular.module('teacherdashboard')
       templateUrl: api.basePrefix + '/components/directives/dashboard/createEditDashboard.html',
       replace: true,
       link: function(scope){
-        scope.flexSize = function(row) {
-          if(row.reports.length === 3) {
-            return 33;
-          } else if(row.reports.length === 2) {
-            return 50;
-          } else {
-            return 100;
+        scope.gridsterOpts = {
+          columns: 6,
+          swapping: true,
+          pushing: true,
+          minSizeX: 2,
+          maxSizeX: 6,
+          minSizeY: 1,
+          maxSizeY: 1
+        };
+
+        scope.dashboardReports = [];
+        scope.$watch(
+          'dashboard',
+          function( newValue, oldValue ) {
+            if(newValue && !angular.equals(newValue, oldValue)) {
+              scope.dashboardReports = scope.processDashboard();
+            }
           }
+        );
+
+        scope.processDashboard = function() {
+          var gridsterData = [];
+          if(scope.dashboard) {
+            for (var m = 0; m < scope.dashboard.rows.length; m++) {
+              var currRow = scope.dashboard.rows[m];
+              for (var n = 0; n < currRow.reports.length; n++) {
+                var currReport = currRow.reports[n];
+                var gElement = {
+                  sizeX: 6 * 1 / currRow.reports.length,
+                  //sizeY: 0.5,
+                  row: m,
+                  col: n * 6 * 1 / currRow.reports.length,
+                  report: currReport
+                };
+                gridsterData.unshift(gElement);
+              }
+            }
+          }
+          return gridsterData;
         };
       }
     };
