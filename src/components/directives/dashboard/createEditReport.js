1bef1bccd6449f85bdac146b3c00a9bec3d02361
markroper <markroper@gmail.com>
2016-03-09 08:35:46 -0500

    Implements initial layout for the report builder.  Still not functional.

--- src/components/directives/dashboard/createEditReport.js
+++ src/components/directives/dashboard/createEditReport.js
@@ -9,7 +9,13 @@ angular.module('teacherdashboard')
       templateUrl: api.basePrefix + '/components/directives/dashboard/createEditReport.html',
       replace: true,
       link: function(scope){
+        scope.xOptions = [ 'assignment date', 'GPA', 'Referrals', 'Failing classes' ];
+        scope.yOptions = [ 'Count of Students', 'SUM referrals'];
+        scope.groupByOptions = ['Race', 'Ethnicity', 'ELL', 'SPED'];
 
+        scope.addBuckets = function() {
+          //TODO:implement
+        }
       }
     };
   }]);
